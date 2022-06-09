c1e02205be9a82d2222b6482ea8ec08e6e6a3178
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-06-09 13:12:54 -0300

    master

---
+++ app.js
@@ -0,0 +1,10 @@
+var nome = 'leno'
+var numero = 22
+var ano = 2022
+console.log(ano - numero)
+console.log(ano + numero)
+console.log( "Estou mexendo no console.log")
+let quartaFeira = false
+console.log(quartaFeira)
+console.log(typeof quartaFeira)
+console.log(nome)
